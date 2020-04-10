856baf38393c07a2b2d34723eddba71f90e7fcd6
higorasilverio <higorasilverio@outlook.com>
2020-04-10 10:18:15 -0300

    Adding two numbers - Final

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Adding two numbers ***");
+		System.out.print("Enter the first value to add: ");
+		double first = scanner.nextDouble();
+		System.out.print("Enter the second value to add: ");
+		double second = scanner.nextDouble();
+		
+		double sum = first + second;
+		
+		System.out.println("Sum = " + sum);
+		
+	}
+
+}
